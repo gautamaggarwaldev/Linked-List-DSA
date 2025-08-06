c4f67d55ae1d14a00193ba362da82429cf3c067d
GautamAggarwalDev <gautam.aggarwal.tech@gmail.com>
2025-08-06 17:53:56 +0530

    create list with random pointer 2nd method

--- 15_CopyListWithRandomPointer.cpp
+++ 15_CopyListWithRandomPointer.cpp
@@ -58,6 +58,67 @@ public:
         }
         return cloneH;
     }
+    // Alternative approach without using a map
+    Node *copyRandomList1(Node *head)
+    {
+        Node *cloneH = NULL;
+        Node *cloneT = NULL;
+
+        Node *temp = head;
+        // step 1: Create a new list with the same values
+        while (temp != NULL)
+        {
+            insertAtTail(cloneH, cloneT, temp->val);
+            temp = temp->next;
+        }
+
+        // step 2: Interleave the original and cloned lists
+        Node *on = head;
+        Node *cn = cloneH;
+        while (on != NULL && cn != NULL)
+        {
+            Node *next = on->next;
+            on->next = cn;
+            on = next;
+
+            next = cn->next;
+            cn->next = on;
+            cn = next;
+        }
+
+        // step 3: Set the random pointers in the cloned list
+        temp = head;
+        while (temp != NULL)
+        {
+            if (temp->next != NULL)
+            {
+                if (temp->random != NULL)
+                {
+                    temp->next->random = temp->random->next;
+                }
+                else
+                {
+                    temp->next->random = NULL;
+                }
+            }
+            temp = temp->next->next;
+        }
+
+        // step 4: Restore the original list and separate the cloned list
+        on = head;
+        cn = cloneH;
+        while (on != NULL && cn != NULL)
+        {
+            on->next = cn->next;
+            on = on->next;
+
+            if (on != NULL)
+                cn->next = on->next;
+            cn = cn->next;
+        }
+        // step 5: Return the cloned list
+        return cloneH;
+    }
 };
 int main()
 {
@@ -69,10 +130,10 @@ int main()
     head->next->next->next->next = new Node(5);
 
     // Setting random pointers
-    head->random = head->next->next; // 1 -> 3
-    head->next->random = head;        // 2 -> 1
-    head->next->next->random = head->next; // 3 -> 2
-    head->next->next->next->random = head; // 4 -> 1
+    head->random = head->next->next;                   // 1 -> 3
+    head->next->random = head;                         // 2 -> 1
+    head->next->next->random = head->next;             // 3 -> 2
+    head->next->next->next->random = head;             // 4 -> 1
     head->next->next->next->next->random = head->next; // 5 -> 2
 
     Node *clonedList = sol.copyRandomList(head);
